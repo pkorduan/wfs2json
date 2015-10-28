9d1d092c8fd65e28187fc3a6467bc039528fd01b
Peter Korduan <peter.korduan@gdi-service.de>
2015-10-28 07:32:08 +0100

    Request WFS, store to filesystem, read into simplexmlelement object, parse feature and attributes.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     'VERSION=1.0.0&' .
     'TYPENAME=' . $config['wfs']['featureType'];
 ?>
-Read GML-Data from WFS with URL<br><a href="<?php echo $get_feature_request; ?>"><?php echo $get_feature_request; ?></a><br>and store the content on the servers filesystem at<br><?php echo getcwd() . '/' . $config['wfs']['localPath'] . $config['wfs']['fileName']; ?><br>
+Read GML-Data from WFS with URL<br><a href="<?php echo $get_feature_request; ?>" target="_blank"><?php echo $get_feature_request; ?></a><br>and store the content on the servers filesystem at<br><?php echo getcwd() . '/' . $config['wfs']['localPath'] . $config['wfs']['fileName']; ?><br>
 <?php
 /*
   $wfs_file = file_get_contents(
@@ -45,95 +45,41 @@ Lese Datei <?php
 echo getcwd() . '/' . $config['wfs']['localPath'] . $config['wfs']['fileName'];
 ?><br>
 <?php
-$wfs_text = file_get_contents(
-  getcwd() .
-  '/' .
-  $config['wfs']['localPath'] .
-  $config['wfs']['fileName']
-);
-$xml = '<?xml version="1.0" encoding="UTF-8" ?>
-<wfs:FeatureCollection
-   xmlns:pflegeportal="http://geoportal.kreis-lup.de/regismv/"
-   xmlns:wfs="http://www.opengis.net/wfs"
-   xmlns:gml="http://www.opengis.net/gml"
-   xmlns:ogc="http://www.opengis.net/ogc"
-   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-   xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd 
-                       http://geoportal.kreis-lup.de/regismv/ http://geoportal.kreis-lup.de/wfs/pflegeportal/sp&amp;SERVICE=WFS?SERVICE=WFS&amp;VERSION=1.0.0&amp;REQUEST=DescribeFeatureType&amp;TYPENAME=pflegeportal:Sozialpflege&amp;OUTPUTFORMAT=XMLSCHEMA">
-    <gml:featureMember>
-      <pflegeportal:Sozialpflege fid="Sozialpflege.3">
-        <gml:boundedBy>
-        	<gml:Box srsName="EPSG:25833">
-        		<gml:coordinates>295317.197594,5950086.015925 295317.197594,5950086.015925</gml:coordinates>
-        	</gml:Box>
-        </gml:boundedBy>
-        <pflegeportal:msGeometry>
-        <gml:Point srsName="EPSG:25833">
-          <gml:coordinates>295317.197594,5950086.015925</gml:coordinates>
-        </gml:Point>
-        </pflegeportal:msGeometry>
-        <pflegeportal:id>3</pflegeportal:id>
-        <pflegeportal:versorgungsart>Gesundheit</pflegeportal:versorgungsart>
-        <pflegeportal:angebot>Arzt</pflegeportal:angebot>
-        <pflegeportal:einrichtung>Brandt, Elke</pflegeportal:einrichtung>
-        <pflegeportal:strasse>Am Mattenstieg</pflegeportal:strasse>
-        <pflegeportal:hnr>1</pflegeportal:hnr>
-        <pflegeportal:plz>19406</pflegeportal:plz>
-        <pflegeportal:gemeinde>Dabel</pflegeportal:gemeinde>
-        <pflegeportal:ortsteil></pflegeportal:ortsteil>
-        <pflegeportal:gemeindeschl>13076026</pflegeportal:gemeindeschl>
-        <pflegeportal:eigentuemer></pflegeportal:eigentuemer>
-        <pflegeportal:traeger></pflegeportal:traeger>
-        <pflegeportal:dachverband></pflegeportal:dachverband>
-        <pflegeportal:ansprechpartner></pflegeportal:ansprechpartner>
-        <pflegeportal:telefon>038485 20424</pflegeportal:telefon>
-        <pflegeportal:email></pflegeportal:email>
-        <pflegeportal:internet></pflegeportal:internet>
-        <pflegeportal:kapazitaet>1</pflegeportal:kapazitaet>
-        <pflegeportal:jahr>2015</pflegeportal:jahr>
-        <pflegeportal:besonderheit></pflegeportal:besonderheit>
-        <pflegeportal:feld20></pflegeportal:feld20>
-        <pflegeportal:aenderung></pflegeportal:aenderung>
-      </pflegeportal:Sozialpflege>
-    </gml:featureMember>
-    <gml:featureMember>
-      <pflegeportal:Sozialpflege fid="Sozialpflege.4">
-        <gml:boundedBy>
-        	<gml:Box srsName="EPSG:25833">
-        		<gml:coordinates>280441.495449,5927796.063287 280441.495449,5927796.063287</gml:coordinates>
-        	</gml:Box>
-        </gml:boundedBy>
-        <pflegeportal:msGeometry>
-        <gml:Point srsName="EPSG:25833">
-          <gml:coordinates>280441.495449,5927796.063287</gml:coordinates>
-        </gml:Point>
-        </pflegeportal:msGeometry>
-        <pflegeportal:id>4</pflegeportal:id>
-        <pflegeportal:versorgungsart>Gesundheit</pflegeportal:versorgungsart>
-        <pflegeportal:angebot>Arzt</pflegeportal:angebot>
-        <pflegeportal:einrichtung>Przybilla, Irene</pflegeportal:einrichtung>
-        <pflegeportal:strasse>Lindenstraße</pflegeportal:strasse>
-        <pflegeportal:hnr>36</pflegeportal:hnr>
-        <pflegeportal:plz>19372</pflegeportal:plz>
-        <pflegeportal:gemeinde>Lewitzrand</pflegeportal:gemeinde>
-        <pflegeportal:ortsteil>Garwitz</pflegeportal:ortsteil>
-        <pflegeportal:gemeindeschl>13076085</pflegeportal:gemeindeschl>
-        <pflegeportal:eigentuemer></pflegeportal:eigentuemer>
-        <pflegeportal:traeger></pflegeportal:traeger>
-        <pflegeportal:dachverband></pflegeportal:dachverband>
-        <pflegeportal:ansprechpartner></pflegeportal:ansprechpartner>
-        <pflegeportal:telefon>038722 20327</pflegeportal:telefon>
-        <pflegeportal:email></pflegeportal:email>
-        <pflegeportal:internet></pflegeportal:internet>
-        <pflegeportal:kapazitaet>1</pflegeportal:kapazitaet>
-        <pflegeportal:jahr>2015</pflegeportal:jahr>
-        <pflegeportal:besonderheit></pflegeportal:besonderheit>
-        <pflegeportal:feld20></pflegeportal:feld20>
-        <pflegeportal:aenderung></pflegeportal:aenderung>
-      </pflegeportal:Sozialpflege>
-    </gml:featureMember>
-  </wfs:FeatureCollection>';
+  $wfs_text = file_get_contents(
+    getcwd() .
+    '/' .
+    $config['wfs']['localPath'] .
+    $config['wfs']['fileName']
+  );
   
+  $sxe = simplexml_load_string($wfs_text);
+
+  ?><pre><?php
+  #echo $sxe->asXml();
+  $featureMembers = $sxe->children('gml', true);
+  foreach($featureMembers AS $featureMember) {
+    if ($featureMember->getName() == 'featureMember') {
+      echo '<hr>';
+      $feature = $featureMember->children('pflegeportal', true);
+      echo 'kategorie: ' . $feature->getName() . '<br>';
+      $attributes = $feature->children('pflegeportal', true);
+      foreach($attributes AS $attribute) {
+        if ($attribute->getName() == 'msGeometry') {
+          $point = $attribute->children('gml', true);
+          $coordinates= $point->children('gml', true);
+          $latlngs = explode(',', (String)$coordinates);
+          echo 'lat: ' . $latlngs[0] . '<br>';
+          echo 'lon: ' . $latlngs[1];
+        }
+        else {
+          echo $attribute->getName() . ' = ' . (String)$attribute;
+        }
+        echo '<br>';
+      }
+    }
+  }
+  ?></pre><?php
+  /*
   $p = xml_parser_create();
   xml_parse_into_struct($p, $xml, $vals, $index);
   xml_parser_free($p);
@@ -143,6 +89,7 @@ $xml = '<?xml version="1.0" encoding="UTF-8" ?>
   echo "\nVals array\n";
   print_r($vals);
   ?></pre><?php
+  */
   
 /*  
 $xml = '<?xml version="1.0" encoding="UTF-8" ?>
